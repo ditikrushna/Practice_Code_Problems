2c8b15e935e6232de44b669c62b11517de73b722
Ditikrushna Giri <42550694+ditikrushna@users.noreply.github.com>
2022-10-15 02:14:20 +0530

    Delete 8kyu.test.py

--- tests/8kyu.test.py
+++
@@ -1,6 +0,0 @@
-def basic_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(dna_to_rna("TTTT"), "UUUU")
-        test.assert_equals(dna_to_rna("GCAT"), "GCAU")
-        test.assert_equals(dna_to_rna("GACCGCCGCC"), "GACCGCCGCC")
